f055b30db2eb61f085c086d1c9a54c21843e0b60
tunogya <tunogya@qq.com>
2020-09-20 15:37:21 +0800

    完善new_transaction

--- utils/Blockchain.py
+++ utils/Blockchain.py
@@ -10,9 +10,22 @@ class Blockchain(object):
         # Creates a new Block and adds it to the chain
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+            向列表中添加一个交易记录，并返回该记录将被添加到的区块的索引
+            :param sender: <str> Address of the Sender
+            :param recipient: <str> Address of the Recipient
+            :param amount: <int> Amount
+            :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
