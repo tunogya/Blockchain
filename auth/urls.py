c4288f2aa9540e664e325a4de8888a57f35a9844
tunogya <tunogya@qq.com>
2020-09-20 19:08:16 +0800

    更新url

--- auth/urls.py
+++ auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    path('auth-token/', obtain_jwt_token),
-    path('token-refresh/', refresh_jwt_token),
-    path('token-verify/', verify_jwt_token),
+    path('token/', obtain_jwt_token),
+    path('refresh/', refresh_jwt_token),
+    path('verify/', verify_jwt_token),
 ]
