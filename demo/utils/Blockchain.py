8933553c5f0eb1c9af294c0cac00c27239994e95
tunogya <tunogya@qq.com>
2020-09-20 16:25:50 +0800

    迁移util

---
+++ demo/utils/Blockchain.py
@@ -0,0 +1,108 @@
+import hashlib
+import json
+from datetime import time
+from urllib.parse import urlparse
+from django.contrib.sites import requests
+
+
+# Blockchain类用来管理链条，它能存储交易，加入新块等
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
+        self.nodes = set()
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    # 向列表中添加一个交易记录，并返回该记录将被添加到的区块的索引
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    # PoW
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    # PoW验证，修改零开头的个数来控制难度
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = str(last_proof * proof).encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:5] == "00000"
+
+    # 检查是否是有效链，遍历每个块验证hash和proof
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    # 用来解决冲突，遍历所有的邻居节点，并用上一个方法检查链的有效性， 如果发现有效更长链，就替换掉自己的链
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get('http://%s/chain' % node)
+
+            if response.status_code == 200:
+                length = json.loads(response)['length']
+                chain = json.loads(response)['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    # 注册节点
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,9 @@
-import json
 from uuid import uuid4
 from django.http import HttpResponse
-from utils.Blockchain import Blockchain
+from demo.utils.Blockchain import Blockchain
+import json
 
 node_identifier = str(uuid4()).replace('-', '')
-
 blockchain = Blockchain()
 
 
@@ -14,20 +13,20 @@ def mine(request):
     proof = blockchain.proof_of_work(last_proof)
     print(proof)
     blockchain.new_transaction(
-         sender="0",
-         recipient=node_identifier,
-         amount=1,
-     )
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
 
     # Forge the new Block by adding it to the chain
     block = blockchain.new_block(proof)
 
     response = {
-         'message': "New Block Forged",
-         'index': block['index'],
-         'transactions': block['transactions'],
-         'proof': block['proof'],
-         'previous_hash': block['previous_hash'],
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
     }
     print(response)
     return HttpResponse(json.dumps(response))
@@ -40,7 +39,7 @@ def new_transaction(request):
         return 'Missing values'
     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
     print(index)
-    response = {'message': 'Transaction will be added to Block %s'%index}
+    response = {'message': 'Transaction will be added to Block %s' % index}
     return HttpResponse(json.dumps(response))
 
 
@@ -50,3 +49,13 @@ def full_chain(request):
         'length': len(blockchain.chain),
     }
     return HttpResponse(json.dumps(response))
+
+
+# 注册节点
+def register_nodes(request):
+    return None
+
+
+# 解决冲突
+def consensus(request):
+    return None
