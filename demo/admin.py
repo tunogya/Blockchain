dcde65809c0b76627d41f5293bb309e444c8afbe
tunogya <tunogya@qq.com>
2020-09-26 14:07:27 +0800

    增加admin

--- demo/admin.py
+++ demo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ['index', 'hash', 'previous_hash', 'create_time', 'proof']
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'type', 'content']
+
+
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'parsed_url']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'state', 'events', 'data']
